b9bd5edb842093f7edddf8818c6d0e00b91f85f8
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-09-23 16:53:09 -0500

    cors configured

---
+++ src/main/java/com/edu/uptc/backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.edu.uptc.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permitir todos los dominios
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permitir todos los encabezados
+                        .allowCredentials(false) // No permite el uso de cookies
+                        .maxAge(3600); // Duración de la política de CORS en segundos
+            }
+        };
+    }
+}
